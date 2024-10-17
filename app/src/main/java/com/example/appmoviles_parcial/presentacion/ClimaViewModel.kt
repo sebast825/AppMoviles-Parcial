b30850d42ebac4a9629bff99a93b07c636660970
sebast825 <sebamolina825@gmail.com>
2024-10-17 20:37:02 -0300

    fun actualizar

--- app/src/main/java/com/example/appmoviles_parcial/presentacion/ClimaViewModel.kt
+++ app/src/main/java/com/example/appmoviles_parcial/presentacion/ClimaViewModel.kt
@@ -10,6 +10,15 @@ class ClimaViewModel {
 
     //es la unica funcion publica que ejecuta el viewModel, el resto son privadas
     fun ejecutar(intencion: ClimaIntencion){
+        when (intencion){
+            ClimaIntencion.actualizar -> actualizar()
+        }
+    }
 
+    private fun actualizar(){
+        estado = ClimaEstado.Cargando
+        //trae datos desde un repo
+
+        estado = ClimaEstado.Error("No funca x ahora")
     }
 }
